3405e2639265f025ef7f9246d0076efa04ce6471
Hassan Hussain <49582324+hassantvs24@users.noreply.github.com>
2020-03-20 01:38:37 +0600

    Send data to another componante
    Props Intigration

--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -4,16 +4,18 @@ import Counter from "./counter";
 class Counters extends Component {
     state = { 
         counters: [
-            {id:1, value: 0},
+            {id:1, value: 4},
             {id:2, value: 0},
             {id:3, value: 0},
-            {id:4, value: 0}
+            {id:4, value: 8}
         ]
      };
     render() { 
         return ( 
             <React.Fragment>
-                {this.state.counters.map(counter => <Counter key={counter.id} />)}
+                {this.state.counters.map(counter => 
+                <Counter key={counter.id} value={counter.value}/>
+                )}
             </React.Fragment> 
         );
     };
