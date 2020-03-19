079697f0d29f27a4674a2cbf51ca3059280590ab
Hassan Hussain <49582324+hassantvs24@users.noreply.github.com>
2020-03-20 04:55:58 +0600

    Impliment Stateless Functional Component
    Stateless Functional Component

--- counter-app/src/components/navbar.jsx
+++ counter-app/src/components/navbar.jsx
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 
-class Navbar extends Component {
-    render() { 
-        return ( 
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <a class="navbar-brand" href="#">
-        Navbar <span className="badge badge-pill secondary">{this.props.totalCounters}</span>
-                </a>
-            </nav> 
-        );
-    }
+//Stateless Functional Component sfc
+
+const Navbar = (props) => {
+    return ( 
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">
+    Navbar <span className="badge badge-pill secondary">{props.totalCounters}</span>
+            </a>
+        </nav> 
+    );
 }
  
 export default Navbar;
