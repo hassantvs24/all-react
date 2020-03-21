99171b1610001ad82f122e7110be35978177d8a5
Hassan Hussain <49582324+hassantvs24@users.noreply.github.com>
2020-03-21 21:05:31 +0600

    Add Exact for fixing route url problem

---
+++ router-app/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import {Route, Switch} from 'react-router-dom';
+import NavBar from "./components/navbar";
+import Products from "./components/products";
+import Posts from "./components/posts";
+import Home from "./components/home";
+import Dashboard from "./components/admin/dashboard";
+import ProductDetails from "./components/productDetails";
+import NotFound from "./components/notFound";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <div className="container">
+          <Route path="/products" component={Products} />
+          <Route path="/posts" component={Posts} />
+          <Route path="/admin" component={Dashboard} />
+          <Route path="/" exact component={Home} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
