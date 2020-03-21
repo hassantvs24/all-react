7f97595218aef7b6e5b13f204378ea6ad3bc6e67
Hassan Hussain <49582324+hassantvs24@users.noreply.github.com>
2020-03-21 23:10:36 +0600

    Implimante Redirect

--- router-app/src/App.js
+++ router-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch, Redirect} from 'react-router-dom';
 import NavBar from "./components/navbar";
 import Products from "./components/products";
 import Posts from "./components/posts";
@@ -19,8 +19,11 @@ class App extends Component {
             <Route path="/products/:id" component={ProductDetails} />
             <Route path="/products" render={(props) => <Products sortBy="newest" {...props} />} />
             <Route path="/posts/:year?/:month?" component={Posts} />{/* ? for making parameter optional */}
+            <Redirect from="/message" to="/posts" />
+            <Route path="/not-found" component={NotFound} />
             <Route path="/admin" component={Dashboard} />
-            <Route path="/" component={Home} />
+            <Route path="/" exact component={Home} />
+            <Redirect to="/not-found" />
           </Switch>
         </div>
       </div>
