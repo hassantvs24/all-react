7e430dd42bb2801c27516439298aeeeb1dd787a1
Hassan Hussain <49582324+hassantvs24@users.noreply.github.com>
2020-03-22 12:43:55 +0600

    Get input dynamicly by name

--- vidly/src/component/loginForm.jsx
+++ vidly/src/component/loginForm.jsx
@@ -18,12 +18,13 @@ class LoginFrom extends Component {
 
     handleChange = e => {
         const account = {...this.state.account};
-        account.username = e.currentTarget.value;
+        account[e.currentTarget.name] = e.currentTarget.value;
         this.setState({account});
 
     };
 
     render() { 
+        const {account} = this.state;
         return ( 
             <div>
                 <h1>Login</h1> 
@@ -31,11 +32,11 @@ class LoginFrom extends Component {
                 <form onSubmit={this.handleSubmit}>
                     <div className="form-group">
                         <label htmlFor="username">Email address</label>
-                        <input type="text" className="form-control" id="username" placeholder="Enter username" value={this.state.account.username} onChange={this.handleChange} />
+                        <input name="username" type="text" className="form-control" id="username" placeholder="Enter username" value={account.username} onChange={this.handleChange} />
                     </div>
                     <div className="form-group">
                         <label htmlFor="password">Password</label>
-                        <input type="password" className="form-control" id="password" placeholder="Password" />
+                        <input name="password" type="password" className="form-control" id="password" placeholder="Password"  value={account.password} onChange={this.handleChange} />
                     </div>
                     <button type="submit" className="btn btn-primary">Login</button>
                 </form>
